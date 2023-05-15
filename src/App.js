64b7f6684abfa20a7885b7fda3824a60ffa3fe62
Karyna Chernyak <mlatech@icloud.com>
2023-05-14 20:53:18 -0500

    set up functions

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./Home"
+
+export default function App(){
+  return(
+    <div>
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+      </Router>
+    </div>
+  )
+}
 
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+export default function Home(){
+const navigate = useNavigate()
+    return(
+        <div>
+
+        </div>
+    )
+}
